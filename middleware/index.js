3a7ee15a59f1e5bc3caaed7ffe635fd2ae396ecc
xuwenjun <156845109@qq.com>
2019-11-19 22:21:14 +0800

    给注册页面增加了一些样式和逻辑

--- middleware/index.js
+++ middleware/index.js
@@ -3,12 +3,12 @@ const bodyParser = require('koa-bodyparser');
 const nunjucks = require('koa-nunjucks-2');
 const static = require('koa-static');
 const mongoose = require('mongoose');
-mongoose.connection.on('open',()=>{
-	console.log('连接上数据库');
-});
-mongoose.connection.on('close',()=>{
-	console.log('断开连接数据库');
-});
+// mongoose.connection.on('open',()=>{
+// 	console.log('连接上数据库');
+// });
+// mongoose.connection.on('close',()=>{
+// 	console.log('断开连接数据库');
+// });
 async function connect () {
   await mongoose.connect('mongodb://localhost:27017/is-u',{ useNewUrlParser: true ,useUnifiedTopology: true });
 }
